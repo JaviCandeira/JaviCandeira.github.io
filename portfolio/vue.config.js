2831d7add1088c19a5184509ec9600f853b138c1
Javier Candeira <javi.candeira@gmail.com>
2023-02-02 18:30:25 +0100

    Update vue.config.js

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  base: '/JaviCandeira.github.io/portfolio',
   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   transpileDependencies: true
 })
